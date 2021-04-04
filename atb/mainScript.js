b3b3e97b5bff49f94d9e7d0c308e1ac2d2cf495d
sarita68470 <sarita68470@gmail.com>
2021-04-04 15:47:18 +0530

    added data

---
+++ atb/mainScript.js
@@ -0,0 +1,47 @@
+var category = [
+{
+    "Director"	         : 	       ""  ,  
+    "Principal"	         : 	       ""  ,  
+    "Dean Academics"	 : 	       ""  ,  
+    "SFIT SC"	         : 	       "https://youtu.be/gfXrXm8P7Y0"  
+},
+{
+    "SFITAA"            :   "https://youtu.be/gfXrXm8P7Y0",
+    "Convenor"          :   "",
+    "Vice President"    :   "",
+    "Secretary"         :   ""
+},
+{
+    "First Year"      :   "https://youtu.be/9SC10xZZdso",
+    "INFT"            :   "https://youtu.be/7vrJhMV37hk",
+    "CMPN"            :   "https://youtu.be/AhUv9-WBeuM",
+    "EXTC"            :   "https://youtu.be/fZnCBO39J1E",
+    "MECH"            :   "https://youtu.be/JceIWTTP7s0",
+    "ELEC"            :   "https://youtu.be/TkiWjNBx8-Y",
+    "Library"         :   "  "
+},
+{
+    "IETE"	 : 	       "https://youtu.be/3iMgopVHeLk"  ,  
+    "CSI"    : 	       "https://youtu.be/KBJLtBCpdmc"  ,  
+    "Ecell"	 : 	       "https://youtu.be/J9FQgVxxRts"  ,  
+    "ISTE"	 : 	       "https://youtu.be/V_hjCNy6pB8"  ,  
+    "NSS"	 : 	       "https://youtu.be/7GR3zH4dqPw"  ,  
+    "IEEE"	 : 	       "https://youtu.be/HjXWyQO7vjY"  ,  
+    "EESA"	 : 	       "https://youtu.be/0FmGpQGxB7M"  ,  
+    "MESA"	 : 	       "https://youtu.be/2f3XYCMj7oA"  ,  
+    "RAW"	 : 	       "https://youtu.be/-8CAG2CYXrw"  ,  
+    "ITSA"	 : 	       "https://youtu.be/JdYeNAykaBE"  ,  
+    "CodeX"	 : 	       "https://youtu.be/FOVFDa0UOtI"  ,  
+    "IIIC"	 :	       "https://youtu.be/E2rCwQdM9Iw"   
+},
+{
+    "Fun Zone"	                 : 	       ""  ,  
+    "Professors Panel"	         : 	       ""  ,  
+    "Student Council Chat"	     : 	       ""  ,  
+    "Game Night"	             : 	       ""  ,  
+    "Ex-Profs"	                 : 	       ""  ,  
+    "Alumni Connect"	         : 	       ""  ,  
+    "Cultural Event"	         : 	       ""    
+}
+
+]
