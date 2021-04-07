64fd292d9b1e8d5e7f152574525b0f8326a73d68
sarita68470 <sarita68470@gmail.com>
2021-04-08 04:10:29 +0530

    update

--- mainScript.js
+++ mainScript.js
@@ -1,47 +1,35 @@
-var category = [
-{
-    "Director"	         : 	       ""  ,  
-    "Principal"	         : 	       ""  ,  
-    "Dean Academics"	 : 	       ""  ,  
-    "SFIT SC"	         : 	       "https://www.youtube.com/embed/gfXrXm8P7Y0"  
-},
-{
-    "SFITAA"            :   "https://www.youtube.com/embed/gfXrXm8P7Y0",
-    "Convenor"          :   "",
-    "Vice President"    :   "",
-    "Secretary"         :   ""
-},
-{
-    "First Year"      :   "https://www.youtube.com/embed/9SC10xZZdso",
-    "INFT"            :   "https://www.youtube.com/embed/7vrJhMV37hk",
-    "CMPN"            :   "https://www.youtube.com/embed/AhUv9-WBeuM",
-    "EXTC"            :   "https://www.youtube.com/embed/fZnCBO39J1E",
-    "MECH"            :   "https://www.youtube.com/embed/JceIWTTP7s0",
-    "ELEC"            :   "https://www.youtube.com/embed/TkiWjNBx8-Y",
-    "Library"         :   "  "
-},
-{
-    "IETE"	 : 	       "https://www.youtube.com/embed/3iMgopVHeLk"  ,  
-    "CSI"    : 	       "https://www.youtube.com/embed/KBJLtBCpdmc"  ,  
-    "Ecell"	 : 	       "https://www.youtube.com/embed/J9FQgVxxRts"  ,  
-    "ISTE"	 : 	       "https://www.youtube.com/embed/V_hjCNy6pB8"  ,  
-    "NSS"	 : 	       "https://www.youtube.com/embed/7GR3zH4dqPw"  ,  
-    "IEEE"	 : 	       "https://www.youtube.com/embed/HjXWyQO7vjY"  ,  
-    "EESA"	 : 	       "https://www.youtube.com/embed/0FmGpQGxB7M"  ,  
-    "MESA"	 : 	       "https://www.youtube.com/embed/2f3XYCMj7oA"  ,  
-    "RAW"	 : 	       "https://www.youtube.com/embed/-8CAG2CYXrw"  ,  
-    "ITSA"	 : 	       "https://www.youtube.com/embed/JdYeNAykaBE"  ,  
-    "CodeX"	 : 	       "https://www.youtube.com/embed/FOVFDa0UOtI"  ,  
-    "IIIC"	 :	       "https://www.youtube.com/embed/E2rCwQdM9Iw"   
-},
-{
-    "Fun Zone"	                 : 	       "fanboard.html"  ,  
-    "Professors Panel"	         : 	       ""  ,  
-    "Student Council Chat"	     : 	       ""  ,  
-    "Game Night"	             : 	       ""  ,  
-    "Ex-Profs"	                 : 	       ""  ,  
-    "Alumni Connect"	         : 	       ""  ,  
-    "Cultural Event"	         : 	       ""    
-}
 
-]
+  $(document).ready(function () {
+    $("#modals_get").load("modals.html");
+    $("#nav_get").load("nav.html");
+    $("#funzoneModal .modal-body").load("forum/forum.html");
+    $("#creditsModal .modal-body").load("credits/credits.html");
+
+    $(".dropdown-item").on("click", function (event) {
+      event.preventDefault();
+      url = $(this).attr("href");
+      img_url = url.split('/embed/')[1];
+
+      value = $(this).html();
+      pages = ["Fun Zone", "Professors Panel", "Student Council Chat", "Game Night", "Cultural Event"];
+      if(jQuery.inArray(value, pages) == -1){
+        $("#youtubeModal .modal-body").html('<iframe  src="' + url + '?rel=0&controls=1&autoplay=1&loop=1&showinfo=0&modestbranding=0"  class="embed-responsive-item youtube-iframe"  title="YouTube video player" loading="lazy"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+        $("#youtubeModal .modal-title").html(value);
+        $('#youtubeModal').modal("show");
+      }
+      else{
+        window.open(url, '_blank');
+      }
+    });
+
+  // changes the iframe src to prevent playback or stop the video playback in our case
+    $("#youtubeModal .close").click(function() {
+      $('.youtube-iframe').each(function(index) {
+        $(this).attr('src', '');
+        return false;
+      });
+      $('#youtubeModal').hide();
+    });
+
+  });
+
